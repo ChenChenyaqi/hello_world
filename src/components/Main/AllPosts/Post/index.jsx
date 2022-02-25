6b869a8635c460850847dedea0e5e9802c9a787d
ChenChenyaqi <15229293783@163.com>
2022-02-25 23:53:22 +0800

    点赞、评论静态效果

--- src/components/Main/AllPosts/Post/index.jsx
+++ src/components/Main/AllPosts/Post/index.jsx
@@ -9,6 +9,7 @@ import localhost from "../../../../utils/localhost";
 const {Paragraph} = Typography
 
 const Post = (props) => {
+    const postId = props.post.postId
     const postAuthor = props.post.postAuthor
     const postTime = props.post.postTime
     const postContent = props.post.postContent
@@ -16,6 +17,35 @@ const Post = (props) => {
     const time = timestampToTime(postTime)
     // 本帖子所有图片路径
     const [picturesPath, setPicturesPath] = React.useState(null)
+    // 点赞数
+    const [likedCount, setLikedCount] = React.useState(0)
+    // 评论数
+    const [commentCount, setCommentCount] = React.useState(0)
+    // 是否已经点过赞
+    const [isLiked, setIsLiked] = React.useState(false)
+
+    // 点赞
+    const liked = (e)=> {
+        e.preventDefault()
+        // 若没点过赞
+        if(!isLiked){
+            setLikedCount(likedCount+1)
+            setIsLiked(true)
+            axios.get(`http://${localhost}:8080/post/liked`,{
+                headers:{
+                    postId
+                }
+            })
+            let arr = JSON.parse(localStorage.getItem("likedPostsId"))
+            arr.push(postId)
+            localStorage.setItem("likedPostsId", JSON.stringify(arr))
+        }
+    }
+
+    // 评论
+    const comment = (e) => {
+        e.preventDefault()
+    }
 
     React.useEffect(() => {
         axios.get(`http://${localhost}:8080/picture/getAll?postId=${props.post.postId}`).then(
@@ -27,7 +57,12 @@ const Post = (props) => {
                 }
             }
         )
-    }, [])
+        // 若没有这个likedPostsId，则为用户本地存储加上
+        let likedPostsId = localStorage.getItem("likedPostsId")
+        if(!likedPostsId){
+            localStorage.setItem("likedPostsId", JSON.stringify([]))
+        }
+    },[])
 
     return (
         <div className="post-wrapper">
@@ -62,11 +97,21 @@ const Post = (props) => {
                     }
 
                 </div>
-                <div style={{height: '20px'}}>
-
-                </div>
-                <div className="post-comment">
-                    {/*<PostComment/>*/}
+                <div className="post-bottom">
+                    <a onClick={liked}>
+                        {
+                            isLiked ? <i className="iconfont icon-dianzan1"/>:
+                                <i className="iconfont icon-dianzan"/>
+                        }
+                        {
+                            likedCount ? likedCount : <span>点赞</span>
+                        }
+                    </a>
+                    <a onClick={comment}><i className="iconfont icon-pinglun"/>
+                        {
+                            commentCount ? commentCount : <span>评论</span>
+                        }
+                    </a>
                 </div>
             </div>
         </div>
